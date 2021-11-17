e286d92845b10f6fbb55eafc9feb6e1fe0077707
hajajmaor <hajajmaor@gmail.com>
2021-11-17 16:18:34 +0200

    private methods add dir for indexes add dir for articles in txt format

--- methods.py
+++ methods.py
@@ -7,14 +7,15 @@ from pickle import loads as load_from_pickle_file
 from pickle import dump as save_to_pickle_file
 from nltk.stem.porter import PorterStemmer
 from position_index_class import PositionalIndex
-INDEX_DIR = "indexes/"
+INDEX_DIR = "./indexes/"
+ARTICLEs_DIR = "./articles/"
 
 with open("stopWordPickle", "rb") as stopWordPickle:
     stop_words: List[str] = load_from_pickle_file(stopWordPickle.read())
 del stopWordPickle
 
 
-def is_stop_word(word: str) -> bool:
+def _is_stop_word(word: str) -> bool:
     """sumary_line: check if the word is a stop word
 
     Keyword arguments:
@@ -24,7 +25,7 @@ def is_stop_word(word: str) -> bool:
     return word in stop_words
 
 
-def do_case_folding(text: str) -> str:
+def _do_case_folding(text: str) -> str:
     """sumary_line: do case folding
 
     Keyword arguments:
@@ -36,18 +37,18 @@ def do_case_folding(text: str) -> str:
     return text.lower()
 
 
-def remove_stop_words(text: str) -> str:
+def _remove_stop_words(text: str) -> str:
     """
     remove stop words from the text
     """
-    text = do_case_folding(text)
+    text = _do_case_folding(text)
     text = text.split(" ")
-    text = [word for word in text if not is_stop_word(word)]
+    text = [word for word in text if not _is_stop_word(word)]
     text = " ".join(text)
     return text
 
 
-def preform_stemming_using_porter(text: str) -> str:
+def _preform_stemming_using_porter(text: str) -> str:
     """
     preform stemming using porter
     """
@@ -58,25 +59,7 @@ def preform_stemming_using_porter(text: str) -> str:
     return text
 
 
-def process_document(doc_id: int, text: str = None, file_name: str = None):
-    """
-        process a document
-    """
-    if file_name:
-        with open(file_name, "r", encoding="utf-8") as file:
-            text = file.read()
-    elif not text:
-        # """if the text and file_name are None"""
-        raise ValueError("Either text or file_name must be provided")
-    # else - there is a text
-    doc_processed = remove_stop_words(text)
-    doc_processed = do_case_folding(doc_processed)
-    doc_processed = preform_stemming_using_porter(doc_processed)
-    index_for_file = generate_index_for_an_article(doc_processed)
-    add_to_index(index_for_file, doc_id)
-
-
-def generate_index_for_an_article(text: str) -> Dict[str, List[int]]:
+def _generate_index_for_an_article(text: str) -> Dict[str, List[int]]:
     """generate index for an article
 
     Args:
@@ -99,17 +82,17 @@ def generate_index_for_an_article(text: str) -> Dict[str, List[int]]:
     return index
 
 
-def add_to_index(index_for_file: Dict[str, List[int]], doc_id: int):
+def _add_to_index(index_for_file: Dict[str, List[int]], doc_id: int):
     """
     add the index to the global index
     """
 
     try:
-        with open("indexPickle", "rb") as index_pickle:
+        with open(f"{INDEX_DIR}indexPickle", "rb") as index_pickle:
             main_index: Dict[str, List[PositionalIndex]] = load_from_pickle_file(
                 index_pickle.read()
             )
-    except [FileNotFoundError, EOFError, ValueError]:
+    except:
         main_index: Dict[str, List[PositionalIndex]] = {}
 
     for word, positions in index_for_file.items():
@@ -118,9 +101,33 @@ def add_to_index(index_for_file: Dict[str, List[int]], doc_id: int):
         else:
             main_index[word].append(PositionalIndex(doc_id, positions))
 
-    index_pickle = open(f"indexPickleAfterDoc{doc_id}", "w")
-    temp_file = open(f"indexPickleAfterDoc{doc_id}", "w")
-    save_to_pickle_file(main_index, index_pickle)
-    save_to_pickle_file(main_index, temp_file)
-    index_pickle.close()
-    temp_file.close()
+    # index_pickle = open(f"{INDEX_DIR}indexPickleAfterDoc{doc_id}", "w")
+    # temp_file = open(f"{INDEX_DIR}indexPickleAfterDoc{doc_id}", "w+")
+    with open(f"{INDEX_DIR}indexPickleAfterDoc{doc_id}", "w+") as index_pickle:
+        with open(f"{INDEX_DIR}indexPickleAfterDoc{doc_id}", "w+") as temp_file:
+            save_to_pickle_file(main_index, index_pickle)
+            save_to_pickle_file(main_index, temp_file)
+    # index_pickle.close()
+    # temp_file.close()
+
+
+def process_document(doc_id: int, text: str = None, file_name: str = None):
+    """
+        process a document
+    """
+    if file_name:
+        with open(file_name, "r", encoding="utf-8") as file:
+            text = file.read()
+    elif not text:
+        # """if the text and file_name are None"""
+        raise ValueError("Either text or file_name must be provided")
+    # else - there is a text
+    doc_processed = _remove_stop_words(text)
+    doc_processed = _do_case_folding(doc_processed)
+    doc_processed = _preform_stemming_using_porter(doc_processed)
+    index_for_file = _generate_index_for_an_article(doc_processed)
+    _add_to_index(index_for_file, doc_id)
+
+
+process_document(
+    1, file_name="articels/Visualizing clustering andpredicting the behavior of museum visitors.txt")
